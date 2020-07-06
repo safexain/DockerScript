926f85c7f90776202bb8467af442c63c1d5a7d2f
safexain <52562552+safexain@users.noreply.github.com>
2020-07-06 10:56:54 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -29,7 +29,4 @@ sudo wget \
         --output-document=/usr/local/bin/docker-compose \
         https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m) \
     && sudo chmod +x /usr/local/bin/docker-compose \
-    && sudo wget \
-        --output-document=/etc/bash_completion.d/docker-compose \
-        "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose" \
     && printf '\nDocker Compose installed successfully\n\n'
